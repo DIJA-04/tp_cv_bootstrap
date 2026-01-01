d518ea6cad589dbb9afb10c40e1b5337ada6a437
Khadija Dieng <khadijadieng1996@gmail.com>
2026-01-01 23:11:54 +0000

    fix: rename Entete -> EnteteComponent, fix styleUrls and update tests

---
+++ cv_bootstrap/src/app/app.ts
@@ -0,0 +1,12 @@
+﻿import { Component, signal } from '@angular/core';
+import { EnteteComponent } from './entete/entete';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  imports: [EnteteComponent],
+  styleUrls: ['./app.scss'],
+})
+export class App {
+  public readonly title = signal('cv_bootstrap');
+}
